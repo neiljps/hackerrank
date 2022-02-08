cf3590a4afe301c86ad2755df8a4b2ea1cde252f
Neil Sardido <neiljps@gmail.com>
2022-02-07 16:39:59 -0800

    Added Cycle Detection

---
+++ cycle-detection/solution.py
@@ -0,0 +1,105 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the has_cycle function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def has_cycle(head):
+    HAS_CYCLE = 1
+    NO_CYCLES = 0
+
+    # Keep track of what nodes we have already visited.
+    nodes_visited = []
+
+    # Constraint: 0 <= list size <= 1000
+    for i in range(0, 1000):
+        # If we encounter the end of the list, then there are no cycles.
+        if(head == None):
+            return NO_CYCLES
+        
+        # If we encounter a node with data that we have seen before,
+        # then there is a cycle.
+        if head in nodes_visited:
+            return HAS_CYCLE
+
+        nodes_visited.append(head)
+        head = head.next    
+
+    return HAS_CYCLE
+
+if __name__ == '__main__':
+    os.environ['OUTPUT_PATH'] = "output.txt" # Added this line
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        index = int(input())
+
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        extra = SinglyLinkedListNode(-1);
+        temp = llist.head;
+
+        for i in range(llist_count):
+            if i == index:
+                extra = temp
+
+            if i != llist_count-1:
+                temp = temp.next
+
+        temp.next = extra
+
+        result = has_cycle(llist.head)
+
+        fptr.write(str(int(result)) + '\n')
+
+    fptr.close()

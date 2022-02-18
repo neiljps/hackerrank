06e301ebea8dbbadaceb8e4222b0fb39cc694ff9
Neil Sardido <neiljps@gmail.com>
2022-02-17 21:33:14 -0800

    Added solution

---
+++ left-rotation/solution.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotateLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def rotateLeft(d, arr):
+    # Write your code here
+    for i in range(0, d):
+        toTheBack = arr[0]
+        arr = arr[1:]
+
+        # then add arr[0] to the end
+        arr.append(toTheBack)
+    return arr
+
+
+if __name__ == '__main__':
+    os.environ['OUTPUT_PATH'] = 'output.txt'
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
